43c5f2f2868babcb5a37bad52bb32266df52f02c
Napol Rachatasumrit <naplr42@gmail.com>
2017-03-25 15:44:57 +0700

    [WitChatAi is broken] implement WitChatAi

--- easybot/libs/chatai/wit.py
+++ easybot/libs/chatai/wit.py
@@ -1,14 +1,23 @@
-from wit import Wit
+import wit
 
-ACCESS_TOKEN=''
+ACCESS_TOKEN = 'NL4Y24EMPAVTPLCQJKRRWKOJ54AAZ4QG'
+
 
 class WitChatAi:
     def __init__(self, botname):
         print("Wit: {}".format(botname))
-        self.client = Wit(access_token=ACCESS_TOKEN)
+        self.client = wit.Wit(access_token=ACCESS_TOKEN)
+        self.req = lambda method, path, params, data: wit.req(self.client.logger, self.client.access_token, method, path, params, **{'data': data})
+
+        # (self.logger, self.access_token, 'GET', '/message', params)
+        # params['context'] = json.dumps(context)
 
     def get_intent(self, msg):
         print('get intent!: {}'.format(msg))
+        params = {}
+        data = {'id': 'hello'}
+        resp = self.req('POST', '/entities', params, data)
+        print('result: {}'.format(str(resp)))
 
     def get_entity(self, msg, entity_type):
         pass
@@ -21,3 +30,4 @@ class WitChatAi:
 
     def add_entity_to_intent(self, entity_name, intent_name):
         pass
+
